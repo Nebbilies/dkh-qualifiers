7613943745e0b8bc6215bb624297dc55f44903fa
nebbilies <hirohamada17092005@gmail.com>
2025-04-21 20:15:18 +0700

    added motion

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,5 @@
-import {useCallback, useEffect, useState} from 'react'
+import {useCallback, useEffect, useState, useReducer} from 'react'
+import {motion} from 'motion/react'
 import data from '/data/info.json'
 
 //input score, add , each 3 digits
@@ -12,9 +13,27 @@ const formatNumber = (number) => {
     return number < 10 ? `0${number}` : number;
 }
 
+const fadeIn = {
+    initial: { opacity: 0, y: 5 },
+    animate: { opacity: 1, y: 0},
+    exit: { opacity: 0, y: 5},
+}
+
+const flyInRotate = {
+    initial: { opacity: 0, y: -100, rotate: 3600},
+    animate: { opacity: 1, y: 0, rotate: 0},
+    exit: { opacity: 0, y: -100 },
+}
+
+const flyIn = {
+    initial: { opacity: 0, y: -20 },
+    animate: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: -20 },
+}
+
 function Home() {
-    const [seed, setSeed] = useState(1)
-    const [seedData, setSeedData] = useState({scores: [], players: []})
+    const [seed, setSeed] = useState(30)
+    const [seedData, setSeedData] = useState({ scores: [], players: []})
     const handleKeyPress = useCallback((event) => {
         if (event.key === 'a' || event.key === 'ArrowLeft') {
                 setSeed((prevSeed) => prevSeed !== 1 ? prevSeed - 1 : prevSeed);
@@ -46,20 +65,38 @@ function Home() {
             <div className='info-container flex w-full h-[864px] gap-[40px]'>
                 <div className={"w-[610px] flex flex-col h-full text-white"}>
                     <div className={'w-full h-[160px] bg-[#89d6c0] rounded-t-[15px] flex items-center p-[20px]'}>
-                        <img src={seedData.icon}
+                        <motion.img key={seedData.icon} src={seedData.icon}
                              className={'w-[120px] h-[120px] rounded-md border-[4px] border-[#243642]'}
-                             alt='team logo'/>
-                        <div className={'ml-[19px] w-[431px] text-wrap max-w-[431px]'}>
+                             alt='team logo'
+                            variants={flyInRotate}
+                                    initial="initial"
+                                    animate="animate"
+                                    exit="exit"
+                                    transition={{ duration: 3, delay: 2 }}
+                        />
+                        <motion.div key={seedData.teamName} className={'ml-[19px] w-[431px] text-wrap max-w-[431px]'}
+                             variants={flyInRotate}
+                             initial="initial"
+                             animate="animate"
+                             exit="exit"
+                             transition={{ duration: 2, delay: 3 }}
+                        >
                             <span
                                 className={'font-bold text-[48px] text-[#243642] text-left break-normal leading-[1.1] overflow-hidden'}>
                                 {seedData.teamName}
                             </span>
-                        </div>
+                        </motion.div>
                     </div>
                     <div
                         className={'w-full flex flex-col h-[400px] bg-[#e2f1e7] rounded-b-[15px] mt-[6px] pt-[14px] pb-[25px] pl-[24px] pr-[24px]'}>
                         {seedData.players.map((player, index) => (
-                            <div key={index} className={'flex flex-row items-center h-[90px] mb-[19px]'}>
+                            <motion.div key={player.username} className={'flex flex-row items-center h-[90px] mb-[19px]'}
+                                variants={flyIn}
+                                initial="initial"
+                                animate="animate"
+                                exit="exit"
+                                transition={{ duration: 0.3, delay: 5.5+0.3*index }}
+                            >
                                 <img src={`https://a.ppy.sh/${player.userId}`} alt="player avatar"
                                      className={'w-[90px] h-[90px] rounded-md border-4 border-[#243642]'}/>
                                 <div className={'flex justify-between w-full'}>
@@ -78,7 +115,7 @@ function Home() {
                                             className={'text-[18px] font-bold -mt-1'}>{player.stats.accuracy}%</span>
                                     </div>
                                 </div>
-                            </div>
+                            </motion.div>
                         ))}
                         <div className={'seed-info flex flex-row self-end text-[#FF5D5D]'}>
                             <span className={'font-semibold text-[39px]'}>SEED</span>
@@ -93,7 +130,7 @@ function Home() {
                 </div>
                 <div className={'maps-info w-[1170px] flex flex-col gap-[16px] h-full text-white'}>
                     {seedData.scores.map((score, index) => (
-                        <div key={index}
+                        <div key={score.beatmapId+seed}
                              className={"flex flex-row w-full h-[64px] rounded-lg bg-[rgba(0,0,0,0)] p-[8px]"}>
                             <a alt="map bg" className={'object-cover h-full w-[190px] rounded-[5px]'}/>
                             <div className={'maps-info-details flex flex-col w-[778px] h-full ml-[15px]'}>
@@ -101,10 +138,16 @@ function Home() {
                                     className={'maps-info-details-name inline-block text-[#387478] font-bold text-[22px]'}>
                                     ‎
                                 </div>
-                                <div
-                                    className={'maps-info-details-scores inline-block font-bold text-[18px] -mt-1.5 text-[#243642]'}>
+                                <motion.div
+                                    className={'maps-info-details-scores inline-block font-bold text-[18px] -mt-1.5 text-[#243642]'}
+                                    variants={fadeIn}
+                                    initial="initial"
+                                    animate="animate"
+                                    exit="exit"
+                                    transition={{ duration: 0.3, delay: 7+0.1*index }}
+                                >
                                     {`${score.players[0].username} • ${formatScore(score.players[0].score)} • ${score.players[0].accuracy}%‎‎‎‎‎‏‏‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎♦‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎${score.players[1].username} • ${formatScore(score.players[1].score)} • ${score.players[1].accuracy}%`}
-                                </div>
+                                </motion.div>
                             </div>
                             <div
                                 className={'maps-info-map-scores flex justify-between items-center bg-[rgba(0,0,0,0)] h-full w-[160px] ml-[9px] rounded-lg'}>
